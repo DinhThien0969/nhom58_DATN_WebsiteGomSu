b90bc02b312c0e48e00945139801ecfefa945a81
tu <tu@gmail.com>
2021-10-23 09:31:48 +0700

    tuanh

---
+++ pottery-shop - Copy/src/main/java/com/potteryshop/PotteryShopApplication.java
@@ -0,0 +1,12 @@
+package com.potteryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotteryShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotteryShopApplication.class, args);
+	}
+}
